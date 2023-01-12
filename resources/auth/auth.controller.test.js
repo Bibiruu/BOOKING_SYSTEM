692a30ebaf6fa52144118fac771f0928a37fc1c6
Maria <so.mariagloria@gmail.com>
2023-01-12 02:08:03 +0200

    Order features created, next => correct mapping

---
+++ resources/auth/auth.controller.test.js
@@ -0,0 +1,2 @@
+const test = require("/.auth.controller.js")
+
--- resources/auth/auth.routes.js
+++ resources/auth/auth.routes.js
@@ -1,12 +1,12 @@
-import { Router } from 'express'
-import { login, register } from './auth.controller.js'
+import { Router } from "express";
+import { login, register } from "./auth.controller.js";
 
-const router = Router()
+const router = Router();
 
 // middleware
 // login users by email
-router.post("/login", login)
+router.post("/login", login);
 
-router.post("/register", register)
+router.post("/register", register);
 
-export default router
+export default router;
